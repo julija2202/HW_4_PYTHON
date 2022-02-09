4af7d284e0988907fc5c5ca169df686d1b1b3dfc
julija2202 <julietkajagolovich@gmail.com>
2022-02-09 16:14:26 +0300

    add for

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 count = 0
-print(count, type(count))
 range_count = 10
 for_count = 0
 run = True
+
 while run:
     print('Hello Cycle')
     break
@@ -27,4 +27,39 @@ while run:
         print('STOP', count),
         break
 
+for item in range(for_count, range_count):
+    print('Steps = ', item)
 
+for item in range(0, 30):
+    if item == 5:
+        print('item = ', item)
+    else:
+        if item == 10:
+            print('item = ', item)
+        else:
+            if item < 4:
+                print('item < ', item)
+            else:
+                if item >= 27:
+                    print('item >= ', item)
+                else:
+                    print('Steps = ', item)
+
+for item in range(0, range_count+1):
+    print('Steps = ', item)
+    if item == 7:
+        inner_count = 0
+        print('--inner_count = ', inner_count)
+        for inner_item in range(0, item):
+            print('-------- Inner_Step = ', inner_item)
+            if inner_item == 5:
+                inner_count = inner_item
+            else:
+                print('-- inner_count =', inner_count)
+
+for item in range(0, 20):
+    print('Steps = ', item)
+    if item > 7 and item < 12:
+        print('If_item =', item)
+        continue
+print('End_iteration =', item)
