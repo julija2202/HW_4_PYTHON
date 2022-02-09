2d6234b2895ac91d63e1db9a53595684f6289d16
julija2202 <julietkajagolovich@gmail.com>
2022-02-09 13:37:52 +0300

    add while

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 count = 0
+print(count, type(count))
 range_count = 10
 for_count = 0
 run = True
 while run:
     print('Hello Cycle')
+    break
 while run:
     print('Step =', count)
     count += 1
+    break
+while count < range_count:
+    print('Step =', count)
+    count += 1
+    break
+while count < range_count:
+    print('Step =', count)
+    count += 1
+    if count == 3:
+        print('Step =', count, 'If body')
+        break
+while run:
+    print('Steps =', count)
+    count += 1
+    if count == range_count:
+        print('STOP', count),
+        break
+
+
